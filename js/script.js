73b3f3c2700ec697476648388ae08b7c4e3ebd0c
sirgama <sirengogamaliel@gmail.com>
2022-02-05 21:01:34 +0300

    test: Scores - Adds percentage scores on results

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 function done() {
     var a = 0;
     var q1 = document.test.quiz1.value;
+    var q2 = document.test.quiz2.value;
+    var q3 = document.test.quiz3.value;
+    var q4 = document.test.quiz4.value;
+    var q5 = document.test.quiz5.value;
 
     if (q1=="ES6") {a++}
+    if (q2=="1995") {a++}
+    if (q3=="Prompt") {a++}
+    if (q4=="Netscape") {a++}
+    if (q5=="Input") {a++}
 
-    document.write(a);
+    var b = a * 20;
+
+    document.write("Your Score is" + ' ');
+    document.write(b, "%");
 }
